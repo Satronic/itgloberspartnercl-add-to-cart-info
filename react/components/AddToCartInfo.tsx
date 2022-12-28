653c6f506edbade110e912b0ec573b056c6a965a
Sneider <electronsgm@gmail.com>
2022-12-28 17:13:09 -0500

    FEAT(/react): Add the useProduct hook

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -1,8 +1,14 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
+import { useProduct } from 'vtex.product-context'
 
 const AddToCartInfo = () => {
-  return <div>This is a basic component</div>
+  const productInfo = useProduct()
+
+  // eslint-disable-next-line no-console
+  console.log('Producto Information: ', productInfo)
+
+  return <div>This is the product info: </div>
 }
 
 export default AddToCartInfo
