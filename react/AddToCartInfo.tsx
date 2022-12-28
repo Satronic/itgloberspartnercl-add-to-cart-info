ee3a197e0666aa1ac1a1fafcb39914fac94d88e1
Sneider <electronsgm@gmail.com>
2022-12-28 16:34:49 -0500

    FEAT(/react): Add a react basic component

---
+++ react/AddToCartInfo.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+import AddToCartInfo from './components/AddToCartInfo'
+
+export default AddToCartInfo
---
+++ react/components/AddToCartInfo.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import React from 'react'
+
+const AddToCartInfo = () => {
+  return <div>This is a basic component</div>
+}
+
+export default AddToCartInfo
