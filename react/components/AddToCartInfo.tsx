51f26c6fe567297d1ae26b9898cd7d99e6b4d9d8
Sneider <electronsgm@gmail.com>
2022-12-28 20:32:10 -0500

    FEAT(/react): Add useOrderForm hook

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -1,12 +1,19 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import { useProduct } from 'vtex.product-context'
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
 
 const AddToCartInfo = () => {
   const productInfo = useProduct()
+  const {
+    orderForm: { items, totalizers },
+  } = useOrderForm()
 
   // eslint-disable-next-line no-console
-  console.log('Producto Information: ', productInfo)
+  console.log('Product Information: ', productInfo)
+
+  // eslint-disable-next-line no-console
+  console.log('Order Information: ', items, totalizers)
 
   return <div>This is the product info: </div>
 }
